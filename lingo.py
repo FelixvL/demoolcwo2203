8dd092b83a05bf8287831becbedeea286944db82
FelixvL <digitaleservice@gmail.com>
2022-03-09 15:18:56 +0100

    dit is de eerste opmerking

---
+++ lingo.py
@@ -0,0 +1,20 @@
+
+def checkOpLetter(woord, lettering):
+    for letter in range(len(woord)):
+        if woord[letter] == lettering:
+            return True
+    return False
+
+
+
+hetjuistewoord = 'fiets'
+print("Welkom bij Lingo")
+print("Raadt het woord")
+print(".....")
+
+for x in range(5):
+    invoer = input("wat is je letter?")
+    if checkOpLetter(hetjuistewoord, invoer):
+        print("je hebt het goed")
+    else:
+        print("je hebt het niet goed")
